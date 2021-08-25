7e5c24b7ab10653418a7f23cb18699b9129c59c1
cauamenezes <cauamenezes.07@gmail.com>
2021-08-25 10:22:24 -0300

    ação quebrar lâmpada

--- lampada.js
+++ lampada.js
@@ -1,16 +1,53 @@
 "use strict";
 
+function lampadaQuebrada() {
+  const lampada = document.getElementById("lampada");
+  return lampada.src.includes("quebrada");
+}
+
 function ligarLampada() {
-  const lampada = document.getElementById("lampada")
-  lampada.src = "img/ligada.jpg"
+  const lampada = document.getElementById("lampada");
+  const botaoLigar = document.getElementById("ligar");
+  const botaoDesligar = document.getElementById("desligar");
+
+  if (!lampadaQuebrada()) {
+    lampada.src = "img/ligada.jpg";
+    botaoLigar.disabled = true;
+    botaoDesligar.disabled = false;
+  }
 }
 
 function desligarLampada() {
-const lampada = document.getElementById("lampada")
-lampada.src = "img/desligada.jpg"
+  const lampada = document.getElementById("lampada");
+  const botaoLigar = document.getElementById("ligar");
+  const botaoDesligar = document.getElementById("desligar");
+
+  if (!lampadaQuebrada()) {
+    lampada.src = "img/desligada.jpg";
+    botaoLigar.disabled = false;
+    botaoDesligar.disabled = true;
+  }
+}
+
+function quebrarLampada() {
+  const lampada = document.getElementById("lampada");
+  const botaoLigar = document.getElementById("ligar");
+  const botaoDesligar = document.getElementById("desligar");
+
+  lampada.src = "img/quebrada.jpg";
+  botaoLigar.disabled = true;
+  botaoDesligar.disabled = true;
 }
 
 //eventos
 document.getElementById("ligar").addEventListener("click", ligarLampada);
 
 document.getElementById("desligar").addEventListener("click", desligarLampada);
+
+document.getElementById("lampada").addEventListener("mouseover", ligarLampada);
+
+document
+  .getElementById("lampada")
+  .addEventListener("mouseleave", desligarLampada);
+
+document.getElementById("lampada").addEventListener("dblclick", quebrarLampada);
